6577c8fd967ab78f936953f57991d838c0653e62
nevaspid <romain.brendle.guido@gmail.com>
2023-01-17 16:20:40 +0100

    commit

--- ft_printf_c.c
+++ ft_printf_c.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf_c.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nevaspid <romain.brendle.guido@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/04 19:23:07 by nevaspid          #+#    #+#             */
-/*   Updated: 2023/01/16 19:39:47 by nevaspid         ###   ########.fr       */
+/*   Created: 2023/01/16 17:25:06 by nevaspid          #+#    #+#             */
+/*   Updated: 2023/01/16 18:56:36 by nevaspid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// man 3 stdarg
-#include "ft_printf.h"
-
-int	conditions(va_list args, const char *str, int i)
+void	ft_putchar_fd(char c, int fd)
 {
-
-}
-
-int ft_printf(const char *str, ...)
-{
-	int len;
-
-	len = 0;
-
-	while (str[len])
-	{
-		write(1, &str[len], 1);
-		len++;
-	}
-	return (len);
+	write(1, &c, 1);
 }
