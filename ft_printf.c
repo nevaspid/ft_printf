f849e517863050c3e492c2b7a05df80fa957da7a
nevaspid <romain.brendle.guido@gmail.com>
2023-02-01 19:17:42 +0100

    commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,58 @@
 /*   By: nevaspid <romain.brendle.guido@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 19:23:07 by nevaspid          #+#    #+#             */
-/*   Updated: 2023/01/31 22:03:31 by nevaspid         ###   ########.fr       */
+/*   Updated: 2023/02/01 19:17:33 by nevaspid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // man 3 stdarg
 #include "ft_printf.h"
 
-int	select_format(va_list args, const char *str, int i)
+static int	select_format(va_list args, const char *str, int i)
 {
+	int len;
 
+	len = 0;
+	if (str[i] == "%")
+		return (null);
+	if (str[i] == 'c')
+		write(1, 'c', 1);
+	if (str[i] == 's')
+		write(1, 's', 1);
+	if (str[i] == 'u')
+		write(1, 'u', 1);
+	if (str[i] == 'x')
+		write(1, 'x', 1);
+	if (str[i] == 'X')
+		write(1, 'X', 1);
+	if (str[i] == 'p')
+		write(1, 'p', 1);
+	if (str[i] == 'd' || str[i] == 'i')
+		write(1, 'di', 2);
+	else
+		i++;
+	return (len);
 }
 
 int ft_printf(const char *str, ...)
 {
 	int i;
+	int error;
+	int count;
 	
 	va_list args;
 
 	i = 0;
+	error = 0;
+	count = 0;
 
 	if (!str)
 		return (null);
 	while (str[i])
 	{
-		if (i == '%')
-			i = select_format(args, *str, i);
+		if (str[i] == '%')
+			count += select_format(args, *str, i);
 		i++;
 	}
-	return (i);
+	return (count);
 }
