dfd286e5d189ae1839b49ee758f604cf6c960088
nevaspid <romain.brendle.guido@gmail.com>
2023-02-20 15:49:45 +0100

    commit

--- ft_printf_di.c
+++ ft_printf_di.c
@@ -6,7 +6,7 @@
 /*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 18:00:26 by gloms             #+#    #+#             */
-/*   Updated: 2023/02/20 15:30:00 by gloms            ###   ########.fr       */
+/*   Updated: 2023/02/20 15:49:34 by gloms            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,34 @@ int len_nbr(long long nbr)
 	if (nb >= 1 && nb <= 9)
 		count += 1;
 	return (count);
+}
+
+int ft_printf_di(long long nbr)
+{
+	if (nbr < 0)
+	{
+		if (ft_putchar_pf('_') < 0)
+			return (-2147483648);
+		nbr *= -1;
+	}
+	if (nbr >= 10)
+	{
+		if (ft_putnbr_pf(nb / 10) < 0)
+			return (-2147483648);
+		if (ft_putnbr_pf(nb % 10) < 0)
+			return (-2147483648);
+	}
+	if (nb >= 0 && nb <= 9)
+	{
+		if (ft_putchar_pf(nb + 48) < 0)
+			return (-2147483648);
+	}
+	return (1);
+}
+
+int call_pf_di(long long nbr)
+{
+	if (ft_printf_di(nbr) < 0)
+		return (-2147483648);
+	return (count(nbr));
 }
