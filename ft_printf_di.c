6ae759947d182704a90aeafa7f81b338e1f7ca03
nevaspid <romain.brendle.guido@gmail.com>
2023-02-20 15:30:13 +0100

    commit

--- ft_printf_di.c
+++ ft_printf_di.c
@@ -6,9 +6,29 @@
 /*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 18:00:26 by gloms             #+#    #+#             */
-/*   Updated: 2023/02/17 21:18:47 by gloms            ###   ########.fr       */
+/*   Updated: 2023/02/20 15:30:00 by gloms            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int len_nbr(long long nbr)
+{
+	int	count;
+
+	count = 0;
+	if (nb <= 0)
+	{
+		count += 1;
+		if (nb < 0)
+			nb *= -1;
+	}
+	while (nb / 10)
+	{
+		nb /= 10;
+		count++;
+	}
+	if (nb >= 1 && nb <= 9)
+		count += 1;
+	return (count);
+}
